57c70153b249072422fc7e452648f51b469b1a3a
robertlandis <16654371+robertlandis@users.noreply.github.com>
2020-12-01 18:11:01 -0600

    part 2 solution

--- 2020/01/robert.js
+++ 2020/01/robert.js
@@ -1,9 +1,16 @@
 const Sum = 2020;
+const DefaultBounds = { LowestValue: 0, HighestValue: 2020 }
 
 $(function () {
 	const SortedInput = SortArray(Input_01);
-	const SolutionPair = FindPairOfInts(SortedInput);
-	Solution = SolutionPair.FirstInt * SolutionPair.SecondInt;
+
+	// part 1
+	const SolutionPair1 = FindPairOfInts(SortedInput, Sum);
+	Solution1 = SolutionPair1.FirstInt * SolutionPair1.SecondInt;
+
+	//part 2
+	const SolutionSet2 = FindSetOfInts(SortedInput);
+	Solution2 = SolutionSet2.FirstInt * SolutionSet2.SecondInt * SolutionSet2.ThirdInt;
 });
 
 function SortArray(InputArray) {
@@ -14,11 +21,40 @@ function SortArray(InputArray) {
 
 // Part 1
 
-function FindPairOfInts(SortedInput) {
+function FindPairOfInts(SortedInput, SumToMatch) {
 	const FirstInt = _.find(SortedInput, (number) => {
-		const Difference = Math.abs(Sum - number);
+		const Difference = Math.abs(SumToMatch - number);
+		if(Difference < SortedInput[0]) {
+			return false;
+		}
 		return _.indexOf(SortedInput, Difference) > -1;
 	});
-	const SecondInt = Sum - FirstInt;
+	const SecondInt = SumToMatch - FirstInt;
 	return { FirstInt, SecondInt };
+}
+
+// Part 2
+
+function FindSetOfInts(SortedInput) {
+	const LowestValue = SortedInput[0];
+	let OtherPair;
+
+	const ThirdInt = _.find(SortedInput, (number, index) => {
+		const Difference = Math.abs(Sum - number);
+		const FilteredInput = _.filter(SortedInput, (number) => { return number < Difference; });
+		if(FilteredInput.length < 2) {
+			return false;
+		}
+
+		if(Difference > LowestValue) {
+			OtherPair = FindPairOfInts(FilteredInput, Difference);
+			if(OtherPair) {
+				return true;
+			}
+		}
+		return false;
+	})
+
+	OtherPair.ThirdInt = ThirdInt;
+	return OtherPair;
 }
