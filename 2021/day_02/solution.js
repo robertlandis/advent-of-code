b50d362c7734643802b68d2fbf729fabe7fdf1a9
robertlandis <16654371+robertlandis@users.noreply.github.com>
2021-12-02 12:30:35 -0600

    day 2

--- 2021/day_02/solution.js
+++ 2021/day_02/solution.js
@@ -1,36 +1,53 @@
 $(function () {
 	// part 1
-	Solution1 = CountNumIncreases_1(Input_01);
+	Solution1 = ParseDirections_1(Input_02);
 
 	// //part 2
-	Solution2 = CountNumIncreases_2(Input_01);
+	Solution2 = ParseDirections_2(Input_02);
 });
 
-function CountNumIncreases_1 (Input) {
-	let NumIncreases = 0;
+function ParseDirections_1(Input) {
+	let Horizontal = Depth = 0;
 
-	_.each(Input, (Value, Index) => {
-		if(Index !== 0) {
-			if(Value > Input[Index - 1]) {
-				NumIncreases++;
-			}	
+	_.each(Input, (Directions) => {
+		let [ Direction, Amount ] = Directions.split(" ");
+		Amount = parseInt(Amount, 10);
+		switch(Direction) {
+			case "up":
+				Depth -= Amount;
+				break;
+			case "forward":
+				Horizontal += Amount;
+				break;
+			case "down":
+				Depth += Amount;
+				break;
 		}
 	});
-	return NumIncreases;
+	return Horizontal * Depth;
 }
 
-function CountNumIncreases_2(Input) {
-	let NumIncreases = 0;
-	const InputSize = _.size(Input);
+function ParseDirections_2(Input) {
+	let Horizontal = (Depth = Aim = 0);
+	console.log({ Horizontal, Depth, Aim });
 
-	_.each(Input, (Value, Index) => {
-		if (Index !== 0 && Index < InputSize - 1) {
-			const Value1 = Input[Index - 1] + Value + Input[Index + 1];
-			const Value2 = Value + Input[Index + 1] + Input[Index + 2];
-			if(Value2 > Value1) {
-				NumIncreases++;
-			}
+	_.each(Input, (Directions) => {
+		let [Direction, Amount] = Directions.split(" ");
+		// console.log({ Direction, Amount });
+		Amount = parseInt(Amount, 10);
+		switch (Direction) {
+			case "up":
+				Aim -= Amount;
+				break;
+			case "forward":
+				Horizontal += Amount;
+				Depth += Aim * Amount;
+				break;
+			case "down":
+				Aim += Amount;
+				break;
 		}
 	});
-	return NumIncreases;
+	console.log({ Horizontal, Depth, Aim });
+	return Horizontal * Depth;
 }
