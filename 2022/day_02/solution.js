24186d9d7289f37c06766f449f58e00399454bd6
Robert Landis <16654371+robertlandis@users.noreply.github.com>
2023-11-30 15:48:58 -0600

    i think it's day 2

---
+++ 2022/day_02/solution.js
@@ -0,0 +1,28 @@
+const MyThrowPoints = {
+	"X": 1,
+	"Y": 2,
+	"Z": 3
+};
+
+const OpponentMapping = {
+	"A": "Rock",
+	"B": "Paper",
+	"C": "Scissors"
+};
+
+const MyMapping = {
+	"X": "Rock",
+	"Y": "Paper",
+	"C": "Scissors"
+};
+
+
+$(function () {
+	// process data
+	const ProcessedData = CalculateElfCalories(Input_01);
+	// part 1
+	// Solution1 = ProcessedData[0];
+
+	// //part 2
+	// Solution2 = DetermineThreeHighestCalElves(ProcessedData);
+});
