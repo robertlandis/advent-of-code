9c10c3c4e6dba077dd8134b276d8c4bef426797f
Robert Landis <16654371+robertlandis@users.noreply.github.com>
2023-12-03 12:18:46 -0600

    day 2 part 2

--- 2023/day2/solution.py
+++ 2023/day2/solution.py
@@ -1,16 +1,16 @@
 import re
 
 maxNumColors = {
-    "Red": 12,
-    "Green": 13,
-    "Blue": 14
+    "red": 12,
+    "green": 13,
+    "blue": 14
 }
 
 def isValidGame(game):
     colorsShown = game.split(",")
     for color in colorsShown:
         [number, thisColor] = color.strip().split()
-        if(int(maxNumColors[thisColor.capitalize()]) < int(number)):
+        if(int(maxNumColors[thisColor]) < int(number)):
             return False
     return True
 
@@ -18,6 +18,14 @@ def getSetsOfGames(lineToParse, currentGame):
     lineToParse = lineToParse.strip()
     lineWithoutGame = lineToParse.replace("Game " + str(currentGame) + ": ", "");
     return lineWithoutGame.split(";")
+
+def getMinColorsByGame(game, minNumByColor):
+    colorsShown = game.split(",")
+    for color in colorsShown:
+        [number, thisColor] = color.strip().split()
+        if(minNumByColor[thisColor] < int(number)):
+            minNumByColor[thisColor] = int(number)
+    return minNumByColor
 
 def part1(input): 
     total = 0
@@ -34,5 +42,19 @@ def part1(input):
         currentGame += 1
     print("Part 1 answer: " + str(total))
 
+def part2(input):
+    total = 0
+    currentGame = 1
+    for line in input:
+        setsOfGames = getSetsOfGames(line, currentGame)
+        minNumByColor = { "red": 0, "blue": 0, "green": 0 }
+        for game in setsOfGames:
+            minNumByColor = getMinColorsByGame(game, minNumByColor)
+        power = minNumByColor["blue"] * minNumByColor["red"] * minNumByColor["green"]
+        total += power
+        currentGame += 1
+    print("Part 2 answer: " + str(total))
+
 with open('input.txt') as input:
-    part1(input)
+    # part1(input)
+    part2(input)
